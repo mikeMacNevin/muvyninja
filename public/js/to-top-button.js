a18fb1f75c601a8dfabc3450e385078468ac5d8a
mikeMacNevin <mikemacnevin@gmail.com>
2021-01-27 20:40:49 -0500

    button to top fix

--- public/js/to-top-button.js
+++ public/js/to-top-button.js
@@ -1,4 +1,40 @@
-$(document).ready(function(){
+//plugin to fix touch/click - https://medium.com/@electrokinetix1/better-touch-vs-click-handling-in-jquery-f974a0144b2
+
+$.fn.touch = function (callback) {
+    $.touchMove = false;
+    $.touch = false;
+    $.touchAction = "";
+    $(this).on("click", function (e) {
+        if (!$.touch) {
+            $.touchAction = 'click';
+            let callbackReal = callback.bind(this);
+            callbackReal(this, e);
+        } else {
+            $.touch = true;
+        }
+        $.touch = false;
+    });
+    $(this).on("touchend", function (e) {
+        $(this).blur();
+        if (typeof e.touches != typeof undefined) {
+            e.preventDefault();
+            $.touch = true;
+            if ($.touchMove) {
+                $.touchMove = false;
+                return false;
+            } else {
+                $.touchAction = 'touch';
+                let callbackReal = callback.bind(this);
+                callbackReal(this, e);
+            }
+        }
+    });
+    $(this).on("touchmove", function (e) {
+        $.touchMove = true;
+    });
+}	
+
+// move 	
 	$(window).scroll(function () {
 			if ($(this).scrollTop() > 80) {
 				$('#back-to-top').fadeIn();
@@ -7,10 +43,9 @@ $(document).ready(function(){
 				$('#back-to-top').fadeOut();
 			}
 		});
-		$('#back-to-top').click(function () {
+		$('#back-to-top').on('click', function () {
 			$('body,html').animate({
 				scrollTop: 0
 			}, 400);
 			return false;
 		});
-});
