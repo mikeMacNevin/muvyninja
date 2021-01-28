fd37492d246c18a6068b36dcabc7dea03cfe9ca6
mikeMacNevin <mikemacnevin@gmail.com>
2021-01-27 22:42:12 -0500

    button fix 4

--- public/js/to-top-button.js
+++ public/js/to-top-button.js
@@ -1,40 +1,45 @@
 //plugin to fix touch/click - https://medium.com/@electrokinetix1/better-touch-vs-click-handling-in-jquery-f974a0144b2
 
-$.fn.touch = function (callback) {
-    $.touchMove = false;
-    $.touch = false;
-    $.touchAction = "";
-    $(this).on("click", function (e) {
-        if (!$.touch) {
-            $.touchAction = 'click';
-            let callbackReal = callback.bind(this);
-            callbackReal(this, e);
-        } else {
-            $.touch = true;
-        }
-        $.touch = false;
-    });
-    $(this).on("touchend", function (e) {
-        $(this).blur();
-        if (typeof e.touches != typeof undefined) {
-            e.preventDefault();
-            $.touch = true;
-            if ($.touchMove) {
-                $.touchMove = false;
-                return false;
-            } else {
-                $.touchAction = 'touch';
-                let callbackReal = callback.bind(this);
-                callbackReal(this, e);
-            }
-        }
-    });
-    $(this).on("touchmove", function (e) {
-        $.touchMove = true;
-    });
-}	
+
+
+// $.fn.touch = function (callback) {
+//     $.touchMove = false;
+//     $.touch = false;
+//     $.touchAction = "";
+//     $(this).on("click", function (e) {
+//         if (!$.touch) {
+//             $.touchAction = 'click';
+//             let callbackReal = callback.bind(this);
+//             callbackReal(this, e);
+//         } else {
+//             $.touch = true;
+//         }
+//         $.touch = false;
+//     });
+//     $(this).on("touchend", function (e) {
+//         $(this).blur();
+//         if (typeof e.touches != typeof undefined) {
+//             e.preventDefault();
+//             $.touch = true;
+//             if ($.touchMove) {
+//                 $.touchMove = false;
+//                 return false;
+//             } else {
+//                 $.touchAction = 'touch';
+//                 let callbackReal = callback.bind(this);
+//                 callbackReal(this, e);
+//             }
+//         }
+//     });
+//     $(this).on("touchmove", function (e) {
+//         $.touchMove = true;
+//     });
+// }	
 
 // move 	
+
+
+$(document).ready(function() {
 	$(window).scroll(function () {
 			if ($(this).scrollTop() > 80) {
 				$('#back-to-top').fadeIn();
@@ -43,9 +48,10 @@ $.fn.touch = function (callback) {
 				$('#back-to-top').fadeOut();
 			}
 		});
-		$('#back-to-top').on('click', function () {
+		$('#back-to-top').on('click touchstart', function () {
 			$('body,html').animate({
 				scrollTop: 0
 			}, 400);
 			return false;
-		});
+		})
+	})
