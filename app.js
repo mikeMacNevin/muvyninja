5e64556e07f68a4a6cbd1bbd5d4d06b1bf8c3ca2
mikeMacNevin <mikemacnevin@gmail.com>
2021-07-17 20:51:16 -0400

    aws push

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const exphbs  = require('express-handlebars');
